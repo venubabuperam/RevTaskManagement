c9826439e111563dd29179fb8ede6db83c41023e
venubabuperam <venubabu119@gmail.com>
2024-06-04 08:20:52 +0530

    backendcommit

--- AngularSpring/src/app/admin.ts
+++
@@ -1,7 +0,0 @@
-export interface Admin {
-
-    name: string;
-    phone:string;
-    email: string;
-    password: string;
-}
--- AngularSpring/src/app/project-manager.ts
+++
@@ -1,6 +0,0 @@
-export interface ProjectManager {
-    id?:number;
-    name:string;
-    email:string;
-    password:string;
-}
